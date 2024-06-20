b078ad4c01a2d83f0d2aece6f53a7577ebcb92dd
ibrahim <ibrahimali139@hotmail.com>
2024-06-20 11:20:51 +0100

    Created User registration page.

--- springboot-registrationform/src/main/java/com/app/springboot_registrationform/controller/CustomerController.java
+++ springboot-registrationform/src/main/java/com/app/springboot_registrationform/controller/CustomerController.java
@@ -1,13 +1,14 @@
-package com.app.controller;
+package com.app.springboot_registrationform.controller;
 
 import com.app.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
-    @GetMapping("/")
+    @RequestMapping("/register")
     public String register() {
         return "register";
     }

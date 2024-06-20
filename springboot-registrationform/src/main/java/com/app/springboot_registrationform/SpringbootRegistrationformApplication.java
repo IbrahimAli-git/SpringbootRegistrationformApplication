0b69cf4abca4a49092cdf1be8df54ca0f6d15f0e
ibrahim <ibrahimali139@hotmail.com>
2024-06-20 08:39:03 +0100

    Initial Commit

---
+++ springboot-registrationform/src/main/java/com/app/springboot_registrationform/SpringbootRegistrationformApplication.java
@@ -0,0 +1,13 @@
+package com.app.springboot_registrationform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegistrationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegistrationformApplication.class, args);
+//		System.out.println("Application");
+	}
+}
---
+++ springboot-registrationform/src/test/java/com/app/springboot_registrationform/SpringbootRegistrationformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springboot_registrationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistrationformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
